ec6bde26e8f37a8aab9c725f6bea129213e5399b
guotie <guotie.9@gmail.com>
2013-12-04 18:15:21 +0800

    remove flash key name param

--- session.go
+++ session.go
@@ -19,26 +19,30 @@ const (
 type Session interface {
 	// Init session by cookie name, fetch data
 	Init()
+
 	// Get returns the session value associated to the given key.
 	Get(key interface{}) interface{}
+
 	// Create a new session ID with sessiondata
 	Create()
+
 	// Set sets the session value associated to the given key.
 	Set(key interface{}, val interface{})
+
 	// Save is to the client, usualy browsers
 	Save() error
+
 	// return should save
 	ShouldSave() bool
+
 	// set should save
 	SetShouldSave(ss bool)
+
 	// AddFlash adds a flash message to the session.
-	// A single variadic argument is accepted, and it is optional: it defines the flash key.
-	// If not defined "_flash" is used by default.
-	AddFlash(value interface{}, vars ...string)
+	AddFlash(value interface{})
+
 	// Flashes returns a slice of flash messages from the session.
-	// A single variadic argument is accepted, and it is optional: it defines the flash key.
-	// If not defined "_flash" is used by default.
-	Flashes(vars ...string) []interface{}
+	Flashes() []interface{}
 }
 
 var (
@@ -47,11 +51,13 @@ var (
 	secretKey   []byte
 )
 
-// Sessions is a Middleware that maps a session.Session service into the Martini handler chain.
+// Sessions is a Middleware that maps a session.Session service into the Martini
+// handler chain.
 // Sessions can use a number of storage solutions with the given store options.
 // store: memory, redis, memcache, etc
 // options: usally json string to open store
-func Sessions(name string, storetype string, dsn string, secret string) martini.Handler {
+func Sessions(name string, storetype string,
+	dsn string, secret string) martini.Handler {
 	var err error
 
 	store, err = Open(storetype, dsn)
@@ -66,7 +72,8 @@ func Sessions(name string, storetype string, dsn string, secret string) martini.
 	}
 	secretKey = []byte(secret)
 
-	return func(res http.ResponseWriter, r *http.Request, c martini.Context, l *log.Logger) {
+	return func(res http.ResponseWriter, r *http.Request, c martini.Context,
+		l *log.Logger) {
 		var (
 			s      *session
 			err    error
@@ -174,28 +181,23 @@ func (s *session) SetShouldSave(ss bool) {
 
 const flashesKey = "_flash"
 
-func (s *session) AddFlash(value interface{}, vars ...string) {
-	key := flashesKey
-	if len(vars) > 0 {
-		key = vars[0]
-	}
+func (s *session) AddFlash(value interface{}) {
 	var flashes []interface{}
-	if v, ok := (*s.data)[key]; ok {
+
+	if v, ok := (*s.data)[flashesKey]; ok {
 		flashes = v.([]interface{})
 	}
-	(*s.data)[key] = append(flashes, value)
+	(*s.data)[flashesKey] = append(flashes, value)
 }
 
-func (s *session) Flashes(vars ...string) []interface{} {
+func (s *session) Flashes() []interface{} {
 	var flashes []interface{}
-	key := flashesKey
-	if len(vars) > 0 {
-		key = vars[0]
-	}
-	if v, ok := (*s.data)[key]; ok {
+
+	if v, ok := (*s.data)[flashesKey]; ok {
 		// Drop the flashes and return it.
-		delete(*s.data, key)
+		delete(*s.data, flashesKey)
 		flashes = v.([]interface{})
 	}
+
 	return flashes
 }
